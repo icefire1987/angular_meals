03a8bf0afd7b46c6732fdce61015372072ecd1e4
cjay <cjay>
2016-06-15 13:46:04 +0200

    start

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularMealsApp
+ * @description
+ * # angularMealsApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('angularMealsApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularMealsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularMealsApp
+ */
+angular.module('angularMealsApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
