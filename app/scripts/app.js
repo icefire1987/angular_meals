937594bb3453099627f8626232abfbc607d6889a
cjay <cjay>
2016-06-17 18:09:53 +0200

    s

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,4 +9,15 @@
  * Main module of the application.
  */
 angular
-  .module('angularMealsApp', []);
+	.module('angularMealsApp', ['ui.router'])
+	.config(['$stateProvider','$urlRouterProvider',function($stateProvider, $urlRouterProvider){
+		$urlRouterProvider.otherwise('/');
+		$stateProvider
+			.state('home', {
+				url: '/',
+				templateUrl: 'views/speisekarte.html',
+				controller: 'MainCtrl as c_main'
+			});
+		
+		
+	}]);
